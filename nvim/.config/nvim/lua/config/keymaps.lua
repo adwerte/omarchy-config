5c37043e744ae579d7e5fef040b885a047338955
Adriaan R. van Roosmalen <vikingvej@hotmail.com>
2025-12-25 12:22:05 +0100

    Updated toggleterm and added nvim-cmp

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,8 +1,3 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- Cargo run in ToggleTerm
-vim.keymap.set("n", "<leader>rr", function()
-  require("lazyvim.util").terminal("cargo run")
-end, { desc = "Cargo run" })
--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,27 @@
 return {
   "akinsho/toggleterm.nvim",
-  version = "*",
+  opts = function(_, opts)
+    -- keep LazyVim's defaults
+    return opts
+  end,
   config = function()
-    require("toggleterm").setup({
-      size = 15,
-      direction = "horizontal",
+    local Terminal = require("toggleterm.terminal").Terminal
+
+    -- Persistent Cargo Run terminal
+    local cargo = Terminal:new({
+      cmd = "clear && cargo run",
+      hidden = true,
       close_on_exit = false,
+      direction = "horizontal",
+      on_open = function()
+        vim.cmd("startinsert!")
+      end,
     })
+
+    -- Keybind: rerun cargo
+    vim.keymap.set("n", "<leader>rr", function()
+      cargo:shutdown()
+      cargo:open()
+    end, { desc = "Cargo run (rerun)" })
   end,
 }
